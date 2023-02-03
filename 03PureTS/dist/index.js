2fa765b03e9b3b739fc719f7d5d1cf836a40bada
Vaibhav Tyagi <VAIBHAVTYAGI438@GMAIL.COM>
2023-02-03 08:30:52 +0530

    classes

--- 03PureTS/dist/index.js
+++ 03PureTS/dist/index.js
@@ -1,3 +1,41 @@
 "use strict";
-console.log("Hi! TypeScript");
-console.log("Hi! TypeScript is Amazing");
+// class User {
+//   public name: string;
+//   private email: string;
+//   readonly city: string = "Delhi";
+//   constructor(name: string, email: string) {
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
+class User {
+  constructor(name, email, userId) {
+    this.name = name;
+    this.email = email;
+    this.userId = userId;
+    // private _courseCount = 1;
+    this._courseCount = 1; //! can access in class also who inherits parent class
+    this.city = "Delhi";
+  }
+  get getAppleEmail() {
+    return `apple${this.email}`;
+  }
+  get courseCount() {
+    return this.courseCount;
+  }
+  set courseCount(courseNum) {
+    if (courseNum <= 1) throw new Error("Course count should be more than 1");
+    this._courseCount = courseNum;
+  }
+}
+class SubUser extends User {
+  constructor() {
+    super(...arguments);
+    this.isFamily = true;
+  }
+  changeCourseCount() {
+    this._courseCount = 7;
+  }
+}
+const vaibhav = new User("vaibhav", "v@ibh.com", "2211");
+// vaibhav.city = "Ahmedabad";
--- 03PureTS/src/index.ts
+++ 03PureTS/src/index.ts
@@ -1,2 +1,45 @@
-console.log("Hi! TypeScript");
-console.log("Hi! TypeScript is Amazing");
+// class User {
+//   public name: string;
+//   private email: string;
+//   readonly city: string = "Delhi";
+
+//   constructor(name: string, email: string) {
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
+class User {
+  // private _courseCount = 1;
+  protected _courseCount = 1; //! can access in class also who inherits parent class
+
+  readonly city: string = "Delhi";
+
+  constructor(
+    public name: string,
+    public email: string,
+    private userId: string
+  ) {}
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this.courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) throw new Error("Course count should be more than 1");
+    this._courseCount = courseNum;
+  }
+}
+
+class SubUser extends User {
+  isFamily: boolean = true;
+  changeCourseCount() {
+    this._courseCount = 7;
+  }
+}
+
+const vaibhav = new User("vaibhav", "v@ibh.com", "2211");
+// vaibhav.city = "Ahmedabad";
